2643e26bc68c199f2224e0aa3ac6333e6d119caa
RoniSpark <roni200906@gmail.com>
2024-07-25 18:59:35 +0200

    fix

--- app/src/main/java/com/example/musiknote/MainActivity.kt
+++ app/src/main/java/com/example/musiknote/MainActivity.kt
@@ -6,52 +6,45 @@ import android.media.MediaRecorder
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
-class SoundRecordingActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     private lateinit var startRecordingButton: Button
     private lateinit var stopRecordingButton: Button
+    private lateinit var result: TextView
     private lateinit var mediaRecorder: MediaRecorder
     private var isRecording = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sound_recording)
+        setContentView(R.layout.activity_main)
 
         startRecordingButton = findViewById(R.id.button_start_recording)
         stopRecordingButton = findViewById(R.id.button_stop_recording)
+        result = findViewById(R.id.result)
 
+/*
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            val permissions = arrayOf(
+                android.Manifest.permission.RECORD_AUDIO,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+            ActivityCompat.requestPermissions(this, permissions, 0)
+        }*/
 
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            val permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
-            ActivityCompat.requestPermissions(this, permissions,0)
-        }
-
-    private fun startRecording() {
-        mediaRecorder = MediaRecorder()
-        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
-        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-        mediaRecorder.setOutputFile("path/to/your/output/file.3gp")
-        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
-
-        try {
-            mediaRecorder.prepare()
-            mediaRecorder.start()
-            isRecording = true
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-    private fun stopRecording() {
-        if (isRecording) {
-            mediaRecorder.stop()
-            mediaRecorder.release()
-            isRecording = false
+        startRecordingButton.setOnClickListener {
+            result.setText("yes")
         }
     }
 }
